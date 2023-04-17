1bf4c2644452e8c759491f729f67e9c46c19d11a
syltur <sylwia.tur99@gmail.com>
2023-04-17 15:44:34 +0200

    add category options

--- src/components/features/PostCard.js
+++ src/components/features/PostCard.js
@@ -2,21 +2,25 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { dateToStr } from '../../utlis/dateToStr';
 
-export function PostCard(props) {
+export function PostCard({ title, author, date, id, description, category }) {
   return (
     <div className='col-12 col-md-6 col-lg-4'>
       <div className='p-3 border border-secondary rounded'>
-        <h3>{props.title}</h3>
+        <h3>{title}</h3>
         <p className='mb-0'>
           <span className='fw-bold'>Author: </span>
-          {props.author}
+          {author}
+        </p>
+        <p className='mb-0'>
+          <span className='fw-bold'>Published: </span>
+          {dateToStr(date)}
         </p>
         <p>
-          <span className='fw-bold'>Published: </span>
-          {dateToStr(props.date)}
+          <span className='fw-bold'>Category: </span>
+          {category}
         </p>
-        <p>{props.description}</p>
-        <Link to={`post/${props.id}`}>
+        <p>{description}</p>
+        <Link to={`post/${id}`}>
           <Button>Read more</Button>
         </Link>
       </div>
--- src/components/features/PostForm.js
+++ src/components/features/PostForm.js
@@ -8,27 +8,42 @@ import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useForm } from 'react-hook-form';
-import { FormLabel, FormControl } from 'react-bootstrap';
+import { FormLabel, FormControl, FormSelect } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { getAllCategories } from '../../redux/categoriesReducer';
 
 export function PostForm({ action, actionText, ...props }) {
-  const [title, setTitle] = useState(props.title ? props.title : '');
-  const [author, setAuthor] = useState(props.author ? props.author : '');
-  const [publishedDate, setDate] = useState(
-    props.publishedDate ? props.publishedDate : ''
+  const { title, author, publishedDate, shortDescription, content, category } =
+    props;
+
+  const [titleState, setTitle] = useState(title || '');
+  const [authorState, setAuthor] = useState(author || '');
+  const [publishedDateState, setDate] = useState(publishedDate || '');
+  const [shortDescriptionState, setDescription] = useState(
+    shortDescription || ''
   );
-  const [shortDescription, setDescription] = useState(
-    props.shortDescription ? props.shortDescription : ''
-  );
-  const [content, setContent] = useState(props.content ? props.content : '');
+  const [contentState, setContent] = useState(content || '');
+  const [categoryState, setCategory] = useState(category || '');
 
   const [contentError, setContentError] = useState(false);
   const [dateError, setDateError] = useState(false);
+  const [categoryError, setCategoryError] = useState(false);
+
+  const allCategories = useSelector(getAllCategories);
 
   const handleSubmit = () => {
-    setContentError(!content);
-    setDateError(!publishedDate);
-    if (content && publishedDate) {
-      action({ title, author, publishedDate, shortDescription, content });
+    setContentError(!contentState);
+    setDateError(!publishedDateState);
+    setCategoryError(!categoryState);
+    if (contentState && publishedDateState && categoryState) {
+      action({
+        title: titleState,
+        author: authorState,
+        publishedDate: publishedDateState,
+        shortDescription: shortDescriptionState,
+        content: contentState,
+        category: categoryState,
+      });
     }
   };
 
@@ -50,7 +65,7 @@ export function PostForm({ action, actionText, ...props }) {
       <FormLabel className='mt-3'>Title</FormLabel>
       <FormControl
         {...register('title', { required: true, minLength: 4 })}
-        value={title}
+        value={titleState}
         onChange={(e) => setTitle(e.target.value)}
         type='text'
         placeholder='Enter title'
@@ -70,7 +85,7 @@ export function PostForm({ action, actionText, ...props }) {
       <FormLabel className='mt-3'>Author</FormLabel>
       <FormControl
         {...register('author', { required: true, minLength: 4 })}
-        value={author}
+        value={authorState}
         onChange={(e) => setAuthor(e.target.value)}
         placeholder='Enter author'
         type='text'
@@ -84,7 +99,7 @@ export function PostForm({ action, actionText, ...props }) {
       <p className='mb-1 mt-3'>Published</p>
       <DatePicker
         className='px-2'
-        selected={publishedDate}
+        selected={publishedDateState}
         onChange={(date) => {
           setDate(date);
         }}
@@ -92,6 +107,28 @@ export function PostForm({ action, actionText, ...props }) {
       {dateError && (
         <small className='d-block form-text text-danger mt-1'>
           This field is required
+        </small>
+      )}
+
+      <FormLabel className='mt-3'>Category</FormLabel>
+      <FormSelect
+        value={categoryState}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value='' disabled>
+          Select category...
+        </option>
+        {allCategories.map((category) => {
+          return (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          );
+        })}
+      </FormSelect>
+      {categoryError && (
+        <small className='d-block form-text text-danger mt-1'>
+          You must pick a category
         </small>
       )}
 
@@ -105,7 +142,7 @@ export function PostForm({ action, actionText, ...props }) {
       <FormLabel className='mt-3'>Short description</FormLabel>
       <FormControl
         {...register('description', { required: true, minLength: 20 })}
-        value={shortDescription}
+        value={shortDescriptionState}
         onChange={(e) => setDescription(e.target.value)}
         placeholder='Leave a comment here'
         as='textarea'
@@ -120,7 +157,7 @@ export function PostForm({ action, actionText, ...props }) {
       <p className='mb-1 mt-3'>Main content</p>
       <ReactQuill
         theme='snow'
-        value={content}
+        value={contentState}
         onChange={setContent}
         placeholder='Leave a comment here'
       ></ReactQuill>
